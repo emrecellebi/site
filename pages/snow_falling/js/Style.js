c980107c449ef7a0d5fb03a4d0aa6fb38928c33a
emrecellebi <Emre_celebi_199505@hotmail.com>
2022-05-07 16:52:10 +0300

    Add Color Snow Falling Page

---
+++ pages/snow_falling/js/Style.js
@@ -0,0 +1,13 @@
+var snoweffect = {
+	"show":"1",
+	"flakes_num":"30",
+	"falling_speed_min":"1",
+	"falling_speed_max":"3",
+	"flake_max_size":"30",
+	"flake_min_size":"10",
+	"vertical_size":"800",
+	"flake_color":"#000000",		// Kar Rengi Orginal Rengi #efefef
+	"flake_zindex":"100000",
+	"flake_type":"#10053",
+	"fade_away":"1"
+};
