b63e8461f58eb8c4b0e0b5b555e83607b2d394c0
emrecellebi <Emre_celebi_199505@hotmail.com>
2022-05-07 16:48:18 +0300

    Add Position Page

---
+++ pages/position/js/style.js
@@ -0,0 +1,15 @@
+﻿function pozisyon(ev) 
+{ 
+	var text = ""; 
+	
+	text += "Pencere Koordinatları (pageX, pageY): " + ev.pageX + ", " + ev.pageY + ".<br />";
+	text += "Ekrana Göre Koordinatlar (screenX, screenY): " + ev.screenX + ", " + ev.screenY + ".<br />"; 
+	text += "Klavye ve Fare KoRdnatları (which): " + ev.which+ ".<br />"; 
+	text += "Olay Tipi (type): " + ev.type + ".<br />"; 
+	
+	document.getElementById("cevap").style.width = 450;
+	document.getElementById("cevap").style.left = ev.pageX; 
+	document.getElementById("cevap").style.top = ev.pageY; 
+	document.getElementById("cevap").innerHTML = text; 
+} 
+document.onmouseup = pozisyon;
